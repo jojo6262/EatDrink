c000e4a03747308ee186269d522885f36555d569
tar <jirapat33022@gmail.com>
2020-04-09 22:54:22 +0700

    database

--- app/src/main/java/com/softwareengineer/eatdrink/CookActivity.java
+++ app/src/main/java/com/softwareengineer/eatdrink/CookActivity.java
@@ -2,13 +2,29 @@ package com.softwareengineer.eatdrink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CookActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer2);
+        setContentView(R.layout.activity_cook);
+        DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mUsersRef = mRootRef.child("users");
+        DatabaseReference mMessagesRef = mRootRef.child("messages");
+        mUsersRef.child("id-1111111").setValue("F");
+        
     }
+
 }

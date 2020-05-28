d3d3c63c8614db4d65903cf9322fb9dc6a7a72e6
USER <ohseed@hotmail.com>
2020-05-29 03:56:54 +0700

    total dailog

--- app/src/main/java/com/softwareengineer/eatdrink/DialogFragment.java
+++ app/src/main/java/com/softwareengineer/eatdrink/DialogFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.softwareengineer.eatdrink.adapter.cartAdapter;
@@ -23,7 +24,7 @@ import java.util.List;
 public class DialogFragment extends Fragment {
 
     RecyclerView rc;
-
+    TextView total;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,9 +37,19 @@ public class DialogFragment extends Fragment {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_dialog, container, false);
         if(MenuActivity.list_img.size()!=0){
             createRecycle(view,MenuActivity.list_order,MenuActivity.list_img,MenuActivity.list_price);
+            int totalprice = 0;
+            for(int i =0;i<MenuActivity.list_price.size();i++){
+                cFragmentPrice cc =  MenuActivity.list_price.get(i);
+                int  price = Integer.parseInt(cc.cFragmentPrice);
+                totalprice= totalprice+price;
+            }
+            System.out.println("////////////"+totalprice);
+            total = view.findViewById(R.id.textView12);
+            total.setText(""+totalprice);
         }else{
             Toast.makeText(getActivity(),"No Order",Toast.LENGTH_LONG );
         }
+
         return view;
 
     }
